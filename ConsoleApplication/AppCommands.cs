ba8d964707101e762c1a55a5a03ad6c606181e68
KHALED ALOMARI <alomari41@gmail.com>
2021-06-01 16:30:34 +0300

    AppCommands.cs ConsoleApplication.cs

--- ConsoleApplication/AppCommands.cs
+++ ConsoleApplication/AppCommands.cs
@@ -15,6 +15,10 @@ namespace CshConsoleAPI
 	{
 		public const string CMD_PROMPED = "\n>";
 		public const string CMD_ECHO = "echo";
+		public const string CMD_ADD = "add";
+		public const string CMD_SUB = "sub";
+		public const string CMD_MULT = "mult";
+		public const string CMD_DIV = "div";
 
 
 
@@ -29,6 +33,47 @@ namespace CshConsoleAPI
 			foreach (string param in parameters){
 				Console.WriteLine(param);
 			}
+			return (true);
+		}
+
+		public static bool CommandAdd(string[] parameters)
+		{
+			int sum = 0;
+			foreach (string param in parameters)
+			{
+				sum += int.Parse(param);
+			}
+			Console.WriteLine(sum);
+			return (true);
+		}
+		public static bool CommandSub(string[] parameters)
+		{
+			int res = 0;
+			foreach (string param in parameters)
+			{
+				res -= int.Parse(param);
+			}
+			Console.WriteLine(res);
+			return (true);
+		}
+		public static bool CommandMult(string[] parameters)
+		{
+			int ab = 1;
+			foreach (string param in parameters)
+			{
+				ab*= int.Parse(param);
+			}
+			Console.WriteLine(ab);
+			return (true);
+		}
+		public static bool CommandDiv(string[] parameters)
+		{
+			int avg = 1;
+			foreach (string param in parameters)
+			{
+				avg /= int.Parse(param);
+			}
+			Console.WriteLine(avg);
 			return (true);
 		}
 	}
